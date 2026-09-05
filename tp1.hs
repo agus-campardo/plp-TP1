3b12da731a475403b9e91f85e367fd2117387418
agus-campardo <aguscampar@gmail.com>
2026-09-04 23:26:43 -0300

    tabs por todos lados 🌟

--- tp1.hs
+++ tp1.hs
@@ -241,43 +241,56 @@ id :: a -> a
 not :: Bool -> Bool
 {NT} not True = False
 {NF} not False = True
+
 ------------------------------------------------------------------------------------------------
 
-Principio de inducción sobre cajas 
+PRINCIPIO DE INDUCCIÓN SOBRE CAJAS: 
 
 data Caja = Bombilla Bool | Nada
 
 Sea P una propiedad sobre expresiones del tipo Caja, basta mostrar que vale: 
-  ▷ ∀b :: Bool. P(b), entonces P(Bombilla b) 
+  ▷ ∀b :: Bool. 
+    P(b), entonces P(Bombilla b) 
   ▷ P(Nada)
 Entonces, vale ∀x :: Caja. P(x). 
 
-
 ------------------------------------------------------------------------------------------------
 
-Principio de inducción sobre circuitos
+PRINCIPIO DE INDUCCIÓN SOBRE CIRCUITOS: (ↈ)
 
 data Circuito = Caja     Caja
               | Serie    Circuito Circuito
               | Paralelo Caja Circuito Circuito Caja
 
-(ↈ)
 Sea P una propiedad sobre expresiones de tipo Circuito basta mostrar que vale: 
-  ▷ ∀caja :: Caja. P(Caja caja)
-  ▷ ∀ci :: Circuito. ∀cd :: Circuito. P(ci) ∧ P(cd), entonces P(Serie ci cd)
-  ▷ ∀ce :: Caja. ∀ci :: Circuito. ∀cd :: Circuito. ∀cs :: Caja. P(ci) ∧ P(cd), entonces P(Paralelo ce ci cd cs)
+  ▷ ∀caja :: Caja. 
+    P(Caja caja)                                                  [caso base]
+  ▷ ∀ci :: Circuito. ∀cd :: Circuito. 
+    P(ci) ∧ P(cd), entonces P(Serie ci cd)                        [caso recursivo]
+  ▷ ∀ce :: Caja. ∀ci :: Circuito. ∀cd :: Circuito. ∀cs :: Caja.                     
+    P(ci) ∧ P(cd), entonces P(Paralelo ce ci cd cs)               [caso recursivo] 
 Entonces, vale ∀x :: Circuito. P(x). 
+
+------------------------------------------------------------------------------------------------
+
+DEMO: 
 
 Queremos demostrar la siguiente propiedad: alternado . alternado = id
 
-Por extensionalidad funcional, nos bastaría demostrar que ∀x :: Circuito. (alternado . alternado) x = id x
-Por {C}, esto es lo mismo que demostar que ∀x :: Circuito. alternado(alternado x) = id x
+Por extensionalidad funcional, nos bastaría demostrar que:  
+  ∀x :: Circuito. (alternado . alternado) x = id x
+
+Por {C}, esto es lo mismo que demostar que: 
+  ∀x :: Circuito. alternado (alternado x) = id x
+
+Definimos P(x) ≡ alternado (alternado x) = id x
 
 Por inducción sobre Circuitos, bastaría con demostrar (ↈ). 
-Siendo P(x): alternado(alternado x) = id x. 
 
-  ▷ ∀caja :: Caja. 
-    P(Caja caja): alternado(alternado (Caja caja)) = id (Caja caja)
+
+
+▷ CASO BASE: Caja 
+  ∀caja :: Caja. P(Caja caja) ≡ alternado(alternado (Caja caja)) = id (Caja caja)
 
       - alternado(alternado (Caja caja)) 
         {AC} = alternado(Caja (cajaAlternada caja))
@@ -328,66 +341,115 @@ Siendo P(x): alternado(alternado x) = id x.
 
               Que era lo que queríamos probar. 
 
+  Por lo tanto, P(Caja caja) queda demostrado. 
 
-  ▷ ∀ci :: Circuito. ∀cd :: Circuito. P(ci) ∧ P(cd), entonces P(Serie ci cd)
+
+
+▷ CASO RECURSIVO: Serie 
+    ∀ci :: Circuito. ∀cd :: Circuito. 
+      P(ci) ∧ P(cd), entonces P(Serie ci cd)
+
+    Hipótesis inductiva: 
+      P(ci) ≡ alternado (alternado ci) = id ci
+      P(cd) ≡ alternado (alternado cd) = id cd
+    
+    Queremos probar: 
     P(Serie ci cd): alternado(alternado (Serie ci cd)) = id (Serie ci cd)
-    HI: P(ci) ∧ P(cd)
 
-    - alternado(alternado (Serie ci cd))
-      {AS} = alternado(Serie (alternado ci) (alternado cd)) 
-      {AS} = Serie (alternado(alternado ci)) (alternado(alternado cd))
-      {HI} = Serie (id ci) (id cd) 
-      {I}  = Serie ci (id cd) 
-      {I}  = Serie ci cd 
-      {I}  = id (Serie ci cd) 
-
-  ▷ ∀ce :: Caja. ∀ci :: Circuito. ∀cd :: Circuito. ∀cd :: Caja. P(ci) ∧ P(cd), entonces P(Paralelo ce ci cd cs)
-    P(Paralelo ce ci cd cs): alternado(alternado (Paralelo ce ci cd cs)) = id (Paralelo ce ci cd cs)
-    HI : P(ci) ∧ P(cd)
-
-    - alternado(alternado (Paralelo ce ci cd cs))
-      {AP} = alternado(Paralelo (cajaAlternada ce) (alternado ci) (alternado cd) (cajaAlternada cs))
-      {AP} = Paralelo (cajaAlternada(cajaAlternada ce)) (alternado(alternado ci)) (alternado(alternado cd)) (cajaAlternada(cajaAlternada cs))
-      {HI} = Paralelo (cajaAlternada(cajaAlternada ce)) (id ci) (id cd) (cajaAlternada(cajaAlternada cs))
-      {LEMA} = Paralelo (id ce) (id ci) (id cd) (id cs)
-      {I}  = Paralelo ce (id ci) (id cd) (id cs)
-      {I}  = Paralelo ce  ci (id cd) (id cs)
-      {I}  = Paralelo ce  ci cd (id cs)
-      {I}  = Paralelo ce ci cd cs 
-      {I}  = id (Paralelo ce ci cd cs)
+    Entonces: 
+      - alternado(alternado (Serie ci cd))
+        {AS} = alternado(Serie (alternado ci) (alternado cd)) 
+        {AS} = Serie (alternado(alternado ci)) (alternado(alternado cd))
+        {HI} = Serie (id ci) (id cd) 
+        {I}  = Serie ci (id cd) 
+        {I}  = Serie ci cd 
+        {I}  = id (Serie ci cd) 
+  
+  Por lo tanto, P(Serie ci cd) queda demostrado. 
 
 
-Lema:  ∀ce :: Caja. cajaAlternada(cajaAlternada c) = id c
-      R(x): cajaAlternada(cajaAlternada c) = id c
-      Por inducción estructural sobre cajas, para probar R(x), basta prbar:
+
+▷ CASO RECURSIVO: Paralelo
+    ∀ce :: Caja. ∀ci :: Circuito. ∀cd :: Circuito. ∀cd :: Caja. 
+      P(ci) ∧ P(cd), entonces P(Paralelo ce ci cd cs)
+
+    Hipótesis inductiva: 
+      P(ci) ≡ alternado (alternado ci) = id ci
+      P(cd) ≡ alternado (alternado cd) = id cd
+    
+    Queremos probar: 
+    P(Paralelo ce ci cd cs) ≡ alternado(alternado (Paralelo ce ci cd cs)) = id (Paralelo ce ci cd cs)
+
+      - alternado(alternado (Paralelo ce ci cd cs))
+        {AP}   = alternado(Paralelo (cajaAlternada ce) (alternado ci) (alternado cd) (cajaAlternada cs))
+        {AP}   = Paralelo (cajaAlternada (cajaAlternada ce)) 
+                          (alternado (alternado ci)) 
+                          (alternado (alternado cd)) 
+                          (cajaAlternada (cajaAlternada cs))
+        {HI}   = Paralelo (cajaAlternada (cajaAlternada ce)) (id ci) (id cd) (cajaAlternada (cajaAlternada cs))
+        {LEMA} = Paralelo (id ce) (id ci) (id cd) (id cs)
+        {I}    = Paralelo ce (id ci) (id cd) (id cs)
+        {I}    = Paralelo ce  ci (id cd) (id cs)
+        {I}    = Paralelo ce  ci cd (id cs)
+        {I}    = Paralelo ce ci cd cs 
+        {I}    = id (Paralelo ce ci cd cs)
+
+  Por lo tanto, P(Paralelo ce ci cd cs) queda demostrado. 
+
+
+
+DEMOSTRACIÓN DEL LEMA:  
+
+  Lema: ∀c :: Caja. cajaAlternada(cajaAlternada c) = id c
+    
+    Sea R(c) ≡ cajaAlternada(cajaAlternada c) = id c
+    
+    Por inducción estructural sobre cajas, para probar R(x), basta probar:
       ▷ ∀b :: Bool. R(Bombilla b)
       ▷ R(Nada)
 
-      ▷ ∀b :: Bool. R(Bombilla b)
-      Por lema de generación de booleanos, b = True o b = False. Luego para probar R(Bombilla b)
-      basta probar R(Bombilla True) y R(Bombilla False).
-        ▷ R(Bombilla True): cajaAlternada(cajaAlternada (Bombilla True)) = id (Bombilla True)
-        cajaAlternada(cajaAlternada (Bombilla True))
-        {CAB} = cajaAlternada (Bombilla (not True))
-        {CAB} = Bombilla (not (not True))
-        {NT}  = Bombilla (not False)
-        {NF}  = Bombilla True
-        {I}   = id (Bombilla True)
+      ▷ CASO: Bombilla 
+          ∀b :: Bool. R(Bombilla b)
 
-        ▷ R(Bombilla False): cajaAlternada(cajaAlternada (Bombilla False)) = id (Bombilla False)
-        cajaAlternada(cajaAlternada (Bombilla False))
-        {CAB} = cajaAlternada (Bombilla (not False))
-        {CAB} = Bombilla (not (not False))
-        {NF}  = Bombilla (not True)
-        {NT}  = Bombilla False
-        {I}   = id (Bombilla False)
+          Por lema de generación de booleanos, b = True o b = False. 
+          Luego para probar R(Bombilla b), basta probar R(Bombilla True) y R(Bombilla False).
 
-      ▷ R(Nada): cajaAlternada(cajaAlternada Nada) = id Nada
-      cajaAlternada(cajaAlternada Nada)
-      {CAN} = cajaAlternada Nada 
-      {CAN} = Nada
-      {I}   = id Nada
-       
+            ▷ Caso b = True
+              R(Bombilla True) ≡ cajaAlternada (cajaAlternada (Bombilla True)) = id (Bombilla True)
+
+              - cajaAlternada(cajaAlternada (Bombilla True))
+                {CAB} = cajaAlternada (Bombilla (not True))
+                {CAB} = Bombilla (not (not True))
+                {NT}  = Bombilla (not False)
+                {NF}  = Bombilla True
+                {I}   = id (Bombilla True)
+
+            ▷ Caso b = False 
+              R(Bombilla False) ≡ cajaAlternada(cajaAlternada (Bombilla False)) = id (Bombilla False)
+
+              - cajaAlternada(cajaAlternada (Bombilla False))
+                {CAB} = cajaAlternada (Bombilla (not False))
+                {CAB} = Bombilla (not (not False))
+                {NF}  = Bombilla (not True)
+                {NT}  = Bombilla False
+                {I}   = id (Bombilla False)
+
+      ▷ CASO: Nada 
+          R(Nada) ≡ cajaAlternada (cajaAlternada Nada) = id Nada
+
+          - cajaAlternada (cajaAlternada Nada)
+            {CAN} = cajaAlternada Nada 
+            {CAN} = Nada
+            {I}   = id Nada
+
+  Por lo tanto, ∀c :: Caja. cajaAlternada (cajaAlternada c) = id c
+
+Hemos demostrado los tres casos del principio de inducción sobre circuitos (ↈ).
+
+Por lo tanto, ∀x :: Circuito. P(x) se cumple.
+
+
+
 
 
     
@@ -397,116 +459,6 @@ Lema:  ∀ce :: Caja. cajaAlternada(cajaAlternada c) = id c
 
                             
 --}
-
-
-
-
-{-
-HECHO CON EL LEMA DE GENERACIÓN 
-
-(ↈ) PRINCIPIO DE INDUCCIÓN ESTRUCTURAL PARA CIRCUITOS: 
-
-data Circuito = Caja     Caja
-              | Serie    Circuito Circuito
-              | Paralelo Caja Circuito Circuito Caja
-
-Para probar P sobre todas las instancias de tipo T, basta probar P para cada uno de los constructores. 
-
-Sea P una propiedad sobre expresiones de tipo Circuito, basta mostrar que vale:
-  ▷ ∀caja :: Caja. P(Caja caja)                                               [caso base]
-  ▷ ∀ci :: Circuito. ∀cd :: Circuito. P(ci) ∧ P(cd), entonces P(Serie ci cd)  [caso inductivo]
-  ▷ ∀ce :: Caja. ∀ci :: Circuito. ∀cd :: Circuito. ∀cs :: Caja.
-    P(ci) ∧ P(cd), entonces P(Paralelo ce ci cd cs)                            [caso inductivo]
-Entonces, vale ∀x :: Circuito. P(x).
-
-
-LEMAS DE GENERACIÓN: 
-Los usaremos para hacer análisis de casos sin inducción . 
-
-Lema de generación para cajas: 
-
-data Caja = Bombilla Bool | Nada
-
-Si caja :: Caja, entonces: 
-  - o bien, caja = Nada 
-  - o bien, ∃b :: Bool. caja = Bombilla b
-
-
-Lemas de generación para booleanos:
-
-data Bool = True | False 
-
-Si b :: Bool, entonces:
-  - o bien, b = True
-  - o bien, b = False 
-
-
-DEMO: 
-
-Queremos demostrostrar: alternado . alternado = id 
-
-Por extensionalidad funcional, nos bastaría demostrar que: 
-  ∀x :: Circuito. (alternado . alternado) x = id x
-
-Por {C}, esto es lo mismo que: 
-  ∀x :: Circuito. alternado(alternado x) = id x
-
-Definimos el predicado P(x) ≡ alternado (alternado x) = id x
-
-Por inducción estructural sobre Circuitos, basta demostrar (ↈ). 
-
-
-CASO BASE: ∀caja :: Caja. P(Caja caja)
-  P(Caja caja): alternado(alternado (Caja caja)) = id (Caja caja)
-
-  - alternado(alternado (Caja caja)) 
-    {AC} = alternado(Caja (cajaAlternada caja))
-    {AC} = Caja (cajaAlternada (cajaAlternada caja))
-  
-  Por lema de generación para cajas, hay dos posibilidades: 
-
-    Caso 1: caja = Nada 
-      
-      Caja (cajaAlternada (cajaAlternada Nada))
-      {CAN} = Caja (cajaAlternada Nada)
-      {CAN} = Caja Nada 
-      {ID}  = id (Caja Nada)
-
-      Entonces, P(Caja Nada) se cumple. 
-
-    Caso 2: ∃b :: Bool. caja = Bombilla b
-
-      Caja (cajaAlternada (cajaAlternada Bombilla b))
-      {CAB} = Caja (cajaAlternada (Bombilla (not b)))
-      {CAB} = Caja (Bombilla (not (not b)))
-
-      Por lema de generación para booleanos, hay dos posibilidades_ 
-
-        Caso 2.1: b = True 
-
-          Caja (Bomnilla (not (not True)))
-          {NT} = Caja (Bombilla (not False))
-          {NT} = Caja (Bombilla True)
-          {ID} = id (Caja (Bombilla True))
-        
-        Caso 2.2: b = False 
-
-          Caja (Bombilla (not (not False)))
-          {NF} = Caja (Bombilla (not True))
-          {NF} = Caja (Bombilla False)
-          {ID} = id (Caja (Bombilla False))
-
-  Por lo tanto, ∀caja :: Caja. P(Caja caja) queda demostrado
-
-  CASO INDUCTIVO: Serie (igual)
-  CASO INDUCTIVO: Paralelo (igual tambien)
-
--}
-
-
-
-
-
 
 
 ejemplo1 = Serie
